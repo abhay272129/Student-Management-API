25396d31c2df0e3f0f5199be53c751827d6f3a22
Abhay Gupta <abhaygupta82764@gmail.com>
2023-07-02 01:56:33 +0530

    updated

--- src/main/java/com/example/owner/developer/StudentManagementReport/StudentController.java
+++ src/main/java/com/example/owner/developer/StudentManagementReport/StudentController.java
@@ -14,22 +14,53 @@ public class StudentController {
    public Student getStudent(@RequestParam("id") int admnNo){//admnNo->request parameter
         return studentsDB.get(admnNo);
     }
+
+    //Add a Student
     @PostMapping("/add")
     public String addStudent(@RequestBody Student student){
        studentsDB.put(student.getAdmnNo(),student);
        return "Student added successfully";
     }
+
     @GetMapping("/get/{id}")
     public Student getStudentByPathVariable(@PathVariable("id") int admnNo){
        return studentsDB.get(admnNo);
     }
+
     @GetMapping("/get/{id}/{message}")
     public String getStudentByMulPathVariable(@PathVariable("id") int admnNo,@PathVariable("message") String message){
         return studentsDB.get(admnNo)+message;
     }
 
+    //Delete a Student by AdmnNo
+    @DeleteMapping("/delete")
+    public String deleteStudent(@RequestParam("id") int admnNo){
+       studentsDB.remove(admnNo);
+       return "Student deleted successfully";
+    }
 
+    //update the course of a studnet based on admnNo
+    @PutMapping("/update")
+    public Student updateCourse(@RequestParam("id") int admnNo,@RequestParam("course") String newCourse){
+       if(!studentsDB.containsKey(admnNo)){
+           throw new RuntimeException("Student doesn't exist");
+       }
+       Student student=studentsDB.get(admnNo);
+       student.setCourse(newCourse);
+       return student;
+    }
 
+    //total number of Students whose age is greater than 20
+    @GetMapping("/get_total_students")
+    public int getTotalStudent(){
+       int total=0;
+       for(int admnNo:studentsDB.keySet()){
+           if(studentsDB.get(admnNo).getAge()>20){
+               total++;
+           }
+       }
+       return total;
+    }
 
 
 }
